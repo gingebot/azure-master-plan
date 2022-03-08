e80b674e9014942ead7671bebe7f388bf8aa6be6
Gingebot <gingebot@gmail.com>
2022-03-08 11:44:29 +0000

    terraform formatting

--- variables.tf
+++ variables.tf
@@ -8,17 +8,17 @@ variable "meh" {
 }
 
 variable "client_id" {
-  type    = string
+  type = string
 }
 
 variable "client_secret" {
-  type    = string
+  type = string
 }
 
 variable "subscription_id" {
-  type    = string
+  type = string
 }
 
 variable "tenant_id" {
-  type    = string
+  type = string
 }
