e37e1e9ad4d2aaefb5a1571f8665549b7b41bc07
Gingebot <gingebot@gmail.com>
2022-03-08 11:42:34 +0000

    setting azure creds

--- variables.tf
+++ variables.tf
@@ -6,3 +6,19 @@ variable "meh" {
   type    = string
   default = "DEVa"
 }
+
+variable "client_id" {
+  type    = string
+}
+
+variable "client_secret" {
+  type    = string
+}
+
+variable "subscription_id" {
+  type    = string
+}
+
+variable "tenant_id" {
+  type    = string
+}
